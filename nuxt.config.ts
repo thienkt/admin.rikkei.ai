e4aeee8bcb164c34c60a1b1d9411ed4a5adc225c
thientk <thienkhac8@gmail.com>
2025-03-01 23:02:22 +0700

    chore: add some tools to automate code tasks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+    },
+  },
+  modules: ['@vuestic/nuxt', '@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    moduleSideEffects: ['reflect-metadata'],
+  },
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
 })
